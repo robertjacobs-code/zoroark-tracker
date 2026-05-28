da54fb2d0a6d8bfb281259bc10fc8e792c6ca5a7
boldstorm-admin <boldstorm@gmail.com>
2026-05-27 20:08:48 -0700

    remove debug session route

--- app/api/debug-session/route.ts
+++
@@ -1,8 +0,0 @@
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/lib/auth-options'
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  const session = await getServerSession(authOptions)
-  return NextResponse.json(session)
-}
